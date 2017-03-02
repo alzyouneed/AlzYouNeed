3669666344f40a88317d2dfabd7dbe37de9673bb
Connor Wybranowski <connor@wybro.com>
2017-03-02 12:01:21 -0500

    Added custom expandableView -Implementing custom view and xib for new notepad

---
+++ AlzYouNeed/notepadView.swift
@@ -0,0 +1,21 @@
+//
+//  notepadView.swift
+//  AlzYouNeed
+//
+//  Created by Connor Wybranowski on 3/2/17.
+//  Copyright © 2017 Alz You Need. All rights reserved.
+//
+
+import UIKit
+
+class notepadView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
